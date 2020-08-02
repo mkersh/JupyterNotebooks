0afb5334178cb019c3e0e0dba18a2a8206e16ea5
Mark Kershaw <mark.kershaw@mambu.com>
2020-08-02 15:02:26 +0100

    update

--- ClojureTests/src/namespaces/ns2.clj
+++ ClojureTests/src/namespaces/ns2.clj
@@ -2,17 +2,31 @@
 ;;; Required namespaces need to be on the classpath
 ;;;     - This is a bit messy for my tests
 (ns namespaces.ns2
-   (:require [namespaces.ns1 :as req1])
+   (:require [namespaces.ns1 :as req1]
+             [clojure.string :as str]
+             [clojure.pprint :as pp])
   )
 ;; Here's another way of requiring another namespace
 ;; but you are better off using :require within (ns ...)
 ;(require '[namespaces.ns1 :as req1])
 
+;; OLD way of doing it that stopped working it newer versions of Java
+;; https://stackoverflow.com/questions/46694600/java-9-compatability-issue-with-classloader-getsystemclassloader
+;; https://blog.codefx.org/java/java-11-migration-guide/#Casting-To-URL-Class-Loader
+(defn XXprintClassPath []
+  (println (seq (.getURLs (java.lang.ClassLoader/getSystemClassLoader)))))
+
+;(require '[clojure.string :as str])
 ;; Print the classpath
 (defn printClassPath []
-(println (seq (.getURLs (java.lang.ClassLoader/getSystemClassLoader)))))
+  (println "JAVA CLASSPATH - Has the following configuration:")
+  (println "===============")
+  (pp/pprint (sort (str/split
+   (System/getProperty "java.class.path")
+   #":")))
+)
 
-;(printClassPath)
+(printClassPath)
 
 (defn ns2SayHello []
   (println "MKNameSpace2 Says Hello from me AND!!!")
