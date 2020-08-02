dfadfbb568ef6ba428843fa3dba5401451aa5f2b
Mark Kershaw <mark.kershaw@mambu.com>
2020-08-02 13:12:43 +0100

    update

--- ClojureTests/src/namespaces/ns2.clj
+++ ClojureTests/src/namespaces/ns2.clj
@@ -2,17 +2,20 @@
 ;;; Required namespaces need to be on the classpath
 ;;;     - This is a bit messy for my tests
 (ns namespaces.ns2
-   (:require [namespaces.ns1])
+   (:require [namespaces.ns1 :as req1])
   )
 
 ;; Print the classpath
-;(println (seq (.getURLs (java.lang.ClassLoader/getSystemClassLoader))))
+(defn printClassPath []
+(println (seq (.getURLs (java.lang.ClassLoader/getSystemClassLoader)))))
+
+;(printClassPath)
 
  ;(require '(ns1))
 
 (defn ns2SayHello []
   (println "MKNameSpace2 Says Hello from me AND!!!")
-  (namespaces.ns1/ns1SayHello)
+  (req1/ns1SayHello)
   )
 
 
