14fee2c09751ecd11e21537c7e26facfcac113fd
mkersh <Jamieson$87654321>
2020-09-03 17:04:16 +0100

    Update

--- ClojureTests/src/state/atom.clj
+++ ClojureTests/src/state/atom.clj
@@ -15,6 +15,9 @@
 (defn fib [n]
   (if (<= n 1)
     n
+    ;; NOTE: recursive calls to fib below may not call this same function!!!
+    ;; After the memoize below they will call the new efficient version
+    ;; This caught me out initially when I tried to change this function name to fib-slow
     (+ (fib (dec n)) (fib (- n 2)))))
 
 (time (fib 35))
@@ -22,3 +25,28 @@
 (def fib (memoize fib))
 
 (time (fib 35))
+
+(comment
+  ;;; We can do a similar thing with function like factorial as well.
+  (defn fac [n]
+    (if (= n 0N)
+      1N
+      (* n (fac (dec n)))))
+
+  (time (fac 100))
+
+  (def fac (memoize fac)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
